3bd7e3318574f96b4b04fce8b241d1e8f3337001
Sam Crowe <crowesam@users.noreply.github.com>
2025-08-01 04:28:08 -0400

    Double image fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
-import Image from 'next/image';
-// import { useUser } from '@stackframe/stack';
 import { 
   CheckCircle, 
   Filter, 
